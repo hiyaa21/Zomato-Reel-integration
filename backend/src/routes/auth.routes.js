50d449c43c52db9a2eaad822d7ace087f07b89c9
hiyaa_21 <2106hiyaa@gmail.com>
2026-05-08 17:09:48 +0530

    Added Logout route

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -5,5 +5,6 @@ const router= express.Router();
 //    /api/auth/user/register
 router.post('/user/register', authController.registerUser);
 router.post('/user/login', authController.loginUser);
+router.get('/user/logout', authController.logoutUser);
 
 module.exports = router;
