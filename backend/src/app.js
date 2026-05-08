85aad046420301421505f39b5dbe70505bf8cf53
hiyaa_21 <2106hiyaa@gmail.com>
2026-05-08 16:06:58 +0530

    Added registration route

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,10 @@
 // Create Server
 const express= require('express');
+const cookieParser= require('cookie-parser');
 
 const app= express();
+app.use(cookieParser());
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello World");
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,9 +1,8 @@
  const express= require('express');
+ const authController= require("../controllers/auth.controller");
 const router= express.Router();
 
 
-router.post('/user/register', (req,res)=>{
-
-})
+router.post('/user/register', authController.registerUser)
 
 module.exports = router;
